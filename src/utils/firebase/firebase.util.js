4f8436563edba2f4ca9191f17c4bcb44fd24f2ee
Omorugie Oyibotha <66238145+RayRugie@users.noreply.github.com>
2024-07-23 03:58:33 +0100

    created checkout page along with checkout items and checkout cart functionality

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -10,7 +10,16 @@ import {
   onAuthStateChanged
 } from 'firebase/auth';
 
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+   getFirestore,
+   doc, 
+   getDoc, 
+   setDoc,
+   collection,
+   writeBatch, 
+   query,
+   getDocs
+  } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCWko33SRrJjEzqhuibDTtVsxPs-g61e90",
@@ -34,6 +43,33 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
   export const db = getFirestore();
+
+  export const  addCollectionAndDocument = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((Object) => {
+      const docRef = doc(collectionRef, Object.title.toLowerCase());
+      batch.set(docRef, Object);
+    });
+
+    await batch.commit();
+    console.log('done');
+  };
+
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+
+    return categoryMap;
+  }
 
   export const createUserDocumentFromAuth = async (
     userAuth, 
